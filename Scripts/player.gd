9d352c40e3dced066f061dd51274eaa16093308d
Frosmoth2324 <99810816+Frosmoth2324@users.noreply.github.com>
2025-04-07 10:36:25 +0200

    light, kinda
    still needs to be circled

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 8000
 @onready var sprite_2d = $Sprite2D
+@onready var light = $PointLight2D
+@onready var oil_timer = $OilTimer
 
+var oil = 15
+
+func _ready():
+	oil_timer.start()
 
 func _physics_process(delta):
 	var direction = Input.get_vector("Left","Right","Up","Down")
@@ -17,4 +23,11 @@ func _physics_process(delta):
 		sprite_2d.flip_h = true
 	move_and_slide()
 
-	move_and_slide()
+
+func _on_oil_timer_timeout():
+	oil -= 1
+	print(oil)
+	if oil > 0:
+		oil_timer.start()
+	else:
+		light.enabled = false
