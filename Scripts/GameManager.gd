15ee85c61f1f0872bab0ffcbd78f7c24a20a4068
Frosmoth2324 <99810816+Frosmoth2324@users.noreply.github.com>
2025-04-07 19:47:31 +0200

    health bar

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,7 +1,9 @@
 extends Node
 
 const MAX_OIL = 15
+const MAX_HEALTH = 100
 var oil = MAX_OIL
+var health = MAX_HEALTH
 var dead = false
 
 func _process(_delta):
--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
 
-@onready var oil_bar = $UI/VBoxContainer/HBoxContainer/OilBar
+@onready var health_bar = $UI/VBoxContainer/Health/HealthBar
+@onready var oil_bar = $UI/VBoxContainer/Oil/OilBar
+
 
 func _ready():
 	oil_bar.max_value = GameManager.MAX_OIL
+	health_bar.max_value = GameManager.MAX_HEALTH
 
 func _process(_delta):
 	oil_bar.value = GameManager.oil
+	health_bar.value = GameManager.health
