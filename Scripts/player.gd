eec3e11a13cfcb79ee56d545881f508138564004
Frosmoth2324 <99810816+Frosmoth2324@users.noreply.github.com>
2025-04-09 11:11:39 +0200

    assets

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,7 @@ const SPEED = 12500
 @onready var light = $PointLight2D
 @onready var oil_timer = $OilTimer
 @onready var animation_player = $AnimationPlayer
+@onready var hit_sound = $HitSound
 
 func _ready():
 	oil_timer.start()
@@ -17,9 +18,13 @@ func _physics_process(delta):
 	else:
 		velocity = Vector2.ZERO
 	if direction.x > 0:
-		sprite_2d.flip_h = false
+		sprite_2d.play("right")
 	elif direction.x < 0:
-		sprite_2d.flip_h = true
+		sprite_2d.play("left")
+	elif direction.y > 0:
+		sprite_2d.play("down")
+	elif direction.y < 0:
+		sprite_2d.play("up")
 	move_and_slide()
 
 func _process(_delta):
@@ -33,3 +38,7 @@ func _on_oil_timer_timeout():
 		oil_timer.start()
 	elif GameManager.oil == 0:
 		animation_player.play("flame_out")
+
+func hit(damage):
+	hit_sound.playing = true
+	GameManager.health -= damage
