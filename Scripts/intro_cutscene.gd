888bb5c6917b2181689e1aeaa03504d8f575221c
Frosmoth2324 <99810816+Frosmoth2324@users.noreply.github.com>
2025-04-13 20:52:27 +0200

    fixed the camera thing

--- Scripts/intro_cutscene.gd
+++ Scripts/intro_cutscene.gd
@@ -2,11 +2,12 @@ extends Control
 @onready var camera_2d: Camera2D = $Player/Camera2D
 @onready var point_light_2d_2: PointLight2D = $PointLight2D2
 @onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var player = $Player
 
 
 
-func _process(delta: float) -> void:
-	if camera_2d.get_screen_center_position() == Vector2(320,1480):
+func _process(_delta):
+	if player.position.y >= 250:
 		await get_tree().create_timer(1).timeout
 		point_light_2d.visible = false
 		get_tree().change_scene_to_file("res://Scenes/intro_cutscene_2.tscn")
